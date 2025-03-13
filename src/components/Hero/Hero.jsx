97f2544b61c5a5e78ffd5013aa80c5a8e39f1704
Jamal Chemlal <chemlal@users.noreply.github.com>
2025-03-13 01:02:46 +0000

    testing image error

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-import { getImageUrl } from "../../utils";
+import myPhoto from "../../../assets/hero/myPhoto.png";
 import styles from "./Hero.module.css";
 
 export const Hero = () => {
@@ -9,7 +9,7 @@ export const Hero = () => {
                 <p className={styles.description}>Ich bin ein autodidaktischer Student, der leidenschaftlich an Programmierung interessiert ist. Ich habe gelernt und lerne weiterhin durch Online-Kurse und Ressourcen. Derzeit bin ich auf der Suche nach einem Ausbildung, um meine Fähigkeiten weiterzuentwickeln und im Bereich der Softwareentwicklung zu wachsen.</p>
                 <a href="mailto:chemlaljamal.job@gmail.com" className={styles.contactBtn}>Kontaktieren Sie mich</a>
             </div>
-            <img src={getImageUrl("hero/myPhoto.jpg")} className={styles.heroImg} alt="hero image" />
+            <img src={myPhoto} className={styles.heroImg} alt="hero image" />
             <div className={styles.topBlur} />
             <div className={styles.bottomBlur} />
         </section>
